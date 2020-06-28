a57e53ce94cbdeb795ca78e0c94a9de241e9d9f6
Amrit Mishra <amrit04092000@gmail.com>
2020-06-29 04:36:21 +0530

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author') 
+
+//all author route
+router.get('/',async(req,res)=>{
+    let searchOption ={}
+    if(req.query.name != null && req.query.name !==''){
+        searchOption.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const authors = await Author.find(searchOption)
+        res.render('authors/index',{
+            authors : authors,
+            searchOption: req.query
+        })
+    }catch{
+        res.redirect('/')
+    }
+    
+})
+
+//New Author routes
+router.get('/new',(req,res) =>{
+    res.render('authors/new',{ author: new Author()})
+})
+
+//Create new Author
+router.post('/',async(req,res)=>{
+    const author = new Author({
+        name: req.body.name
+    })
+    try{
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch{
+        res.render('authors/new',{
+            author: author,
+            errorMessage: 'Error creatig Author'
+        }) 
+    }
+})
+module.exports = router
